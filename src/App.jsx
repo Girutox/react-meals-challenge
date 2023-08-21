c8dae21e9d8b135dd4e63357c8b7c3e47704bb54
Giancarlo Illescas <giancarlo.hoyos@gmail.com>
2023-08-21 12:03:00 -0500

    V4

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import Header from "./components/Header/Header";
+import MealsImg from './assets/meals.jpg';
+
+import styles from './App.module.scss';
+import Meals from "./components/Meals/Meals";
 
 function App() {
   return (
     <>
       <Header />
+      <main>
+        <div className={styles['main-image']}>
+          <img src={MealsImg} alt="A table full of food" />
+        </div>
+        <Meals />
+      </main>
     </>
   );
 }
